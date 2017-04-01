3bbee07bf436c00fa47ace6e5944e697b4ff8501
qioixiy <qioixiy@gmail.com>
2017-04-02 00:49:04 +0800

    Result.doc.ftl ok

--- src/test/java/com/xxx/utils/freemaker/ExportDocTest.java
+++ src/test/java/com/xxx/utils/freemaker/ExportDocTest.java
@@ -35,9 +35,38 @@ public class ExportDocTest {
 	@Test
 	public void exportResultTest() {
 		Map<String, Object> dataMap = new HashMap<String, Object>();
-		String fileName = "/tmp/Result.doc";
+		String fileName = "Result.doc";
 		String template = "Result.doc.ftl";
-		dataMap.put("word", "hello");
+		dataMap.put("v_kechongmingcheng", "hello");
+		dataMap.put("v_jiaoxuequdui", "hello");
+		dataMap.put("v_kaoshirenshu", "hello");
+		dataMap.put("v_90_100_fen", "hello");
+		dataMap.put("v_80_89_fen", "hello");
+		dataMap.put("v_70_79_fen", "hello");
+		dataMap.put("v_60_69_fen", "hello");
+		dataMap.put("v_quekaorenshu", "hello");
+		dataMap.put("v_90_100_baifenbi", "hello");
+		dataMap.put("v_80_89_baifenbi", "hello");
+		dataMap.put("v_70_79_baifenbi", "hello");
+		dataMap.put("v_60_69_baifenbi", "hello");
+		dataMap.put("v_60_yixia_fen", "hello");
+		dataMap.put("v_zuigaofen", "hello");
+		dataMap.put("v_zuidifen", "hello");
+		dataMap.put("v_pingjunfen", "hello");
+		dataMap.put("v_60_yixia_baifenbi", "hello");
+		dataMap.put("v_shiti_jichu_baifenbi", "hello");
+		dataMap.put("v_shiti_jichu_defenqingkuang", "hello");
+		dataMap.put("v_shiti_zhongdeng_baifenbi", "hello");
+		dataMap.put("v_shiti_zhongdeng_defenqingkuang", "hello");
+		dataMap.put("v_yidingnandu_baifenbi", "hello");
+		dataMap.put("v_yidingnandu_defenqingkuang", "hello");
+		dataMap.put("v_shitijieguoyuchengjizhonghefenxi", "hello");
+		dataMap.put("v_chengjifenbu_90_100", 100);
+		dataMap.put("v_chengjifenbu_80_89", 200);
+		dataMap.put("v_chengjifenbu_70_79", 300);
+		dataMap.put("v_chengjifenbu_70_79_2", 300);
+		dataMap.put("v_chengjifenbu_60_69", 400);
+		dataMap.put("v_chengjifenbu_60_yixia", 500);
 
 		try {
 			mExportDoc.createDoc(dataMap, fileName, template);
