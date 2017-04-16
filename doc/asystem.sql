ce8f2aad75d5f7dc59ff00ff1dfc340e666f09ea
qioixiy <qioixiy@gmail.com>
2017-04-16 18:54:49 +0800

    doc/asystem.sql

--- doc/asystem.sql
+++ doc/asystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 2017-04-09 12:19:54
+-- Generation Time: 2017-04-16 18:53:58
 -- 服务器版本： 5.7.17-0ubuntu1
 -- PHP Version: 7.0.15-1ubuntu4
 
@@ -39,20 +39,10 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`id`, `name`, `title`, `detail`, `creater`, `create_timestamp`) VALUES
-(1, '数学基础', '数学基础知识', '学习数学基础知识', 0, '2017-03-30 13:18:55'),
-(2, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(3, '数学基础', '数学基础知识', '学习数学基础知识', 0, '2017-03-30 13:18:55'),
-(4, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(5, '数学基础', '数学基础知识', '学习数学基础知识', 0, '2017-03-30 13:18:55'),
-(6, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(7, '数学基础', '数学基础知识', '学习数学基础知识', 0, '2017-03-30 13:18:55'),
-(8, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(9, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(10, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(11, '数学基础', '数学基础知识', '学习数学基础知识', 0, '2017-03-30 13:18:55'),
-(12, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(13, '计算机基础', '计算机', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
-(14, '物理', '物理现象', '物理存在', 1, '2017-04-09 03:18:09');
+(1, '数学', '数学基础知识', '学习数学基础知识', 2, '2017-04-11 14:29:22'),
+(2, '大学计算机基础', '计算机基础', '计算机基础知识介绍', 1, '2017-03-30 13:19:13'),
+(15, '语文', '文学基础', '文学基础', 1, '2017-04-16 10:26:50'),
+(14, '物理', '自然规律', '物理存在', 1, '2017-04-09 03:18:09');
 
 -- --------------------------------------------------------
 
@@ -73,7 +63,8 @@ CREATE TABLE `score_result` (
 --
 
 INSERT INTO `score_result` (`id`, `student_id`, `teacher_id`, `test_paper_id`, `detail`) VALUES
-(1, 1, 1, 1, '{}');
+(13, 2, 2, 2, '{\"student_id\":\"2\",\"teacher_id\":\"2\",\"paper_id\":\"2\",\"paper_name\":\"2017物理期末试卷\",\"paper_total_score\":\"100\",\"data\":[{\"type\":\"选择题1\",\"level_str\":\"基础题目\",\"level_index\":\"1\",\"score_total\":\"20\",\"score_real\":\"10\",\"comments\":\"\"},{\"type\":\"选择题2\",\"level_str\":\"高难度题目\",\"level_index\":\"3\",\"score_total\":\"30\",\"score_real\":\"20\",\"comments\":\"\"},{\"type\":\"计算题1\",\"level_str\":\"中等难度\",\"level_index\":\"2\",\"score_total\":\"20\",\"score_real\":\"20\",\"comments\":\"\"},{\"type\":\"计算题2\",\"level_str\":\"高难度题目\",\"level_index\":\"3\",\"score_total\":\"30\",\"score_real\":\"20\",\"comments\":\"\"}]}'),
+(12, 1, 1, 1, '{\"student_id\":\"1\",\"teacher_id\":\"1\",\"paper_id\":\"1\",\"paper_total_score\":\"120\",\"data\":[{\"type\":\"选择题1\",\"level_str\":\"基础题目\",\"level_index\":\"1\",\"score_total\":\"5\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"选择题2\",\"level_str\":\"基础题目\",\"level_index\":\"1\",\"score_total\":\"5\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"选择题3\",\"level_str\":\"基础题目\",\"level_index\":\"1\",\"score_total\":\"5\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"选择题4\",\"level_str\":\"中等难度\",\"level_index\":\"2\",\"score_total\":\"15\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"选择题5\",\"level_str\":\"高难度题目\",\"level_index\":\"3\",\"score_total\":\"20\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"填空题1\",\"level_str\":\"基础题目\",\"level_index\":\"1\",\"score_total\":\"10\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"填空题2\",\"level_str\":\"中等难度\",\"level_index\":\"2\",\"score_total\":\"10\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"简答题1\",\"level_str\":\"基础题目\",\"level_index\":\"1\",\"score_total\":\"10\",\"score_real\":\"1\",\"comments\":\"\"},{\"type\":\"简答题2\",\"level_str\":\"高难度题目\",\"level_index\":\"3\",\"score_total\":\"20\",\"score_real\":\"1\",\"comments\":\"\"}]}');
 
 -- --------------------------------------------------------
 
@@ -84,6 +75,7 @@ INSERT INTO `score_result` (`id`, `student_id`, `teacher_id`, `test_paper_id`, `
 CREATE TABLE `student` (
   `id` int(11) NOT NULL COMMENT 'id',
   `name` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(1024) DEFAULT NULL COMMENT '密码',
   `number` varchar(40) NOT NULL COMMENT '编号：学生的学号',
   `email` varchar(40) NOT NULL DEFAULT 'a@b.com' COMMENT '联系方式:email',
   `telphone` varchar(20) NOT NULL COMMENT '联系方式:telphone'
@@ -93,11 +85,12 @@ CREATE TABLE `student` (
 -- 转存表中的数据 `student`
 --
 
-INSERT INTO `student` (`id`, `name`, `number`, `email`, `telphone`) VALUES
-(1, '小天', '001', 'a@b.com', '18000008888'),
-(2, '小张', '002', 'a@b.com', '18000008888'),
-(9, '小还', '004', 'z@c.com', '17099999999'),
-(8, '小王', '003', 'c@d.com', '18011111111');
+INSERT INTO `student` (`id`, `name`, `password`, `number`, `email`, `telphone`) VALUES
+(1, '呜呜', NULL, '20170901', '1@s.com', '19000000001'),
+(2, '小张', NULL, '20170902', '2@s.com', '19000000002'),
+(9, '小还', NULL, '20170903', '3@s.com', '19000000003'),
+(8, '小王', NULL, '20170904', '4@s.com', '19000000004'),
+(12, '张一', NULL, '20170905', '5@s.com', '19000000005');
 
 -- --------------------------------------------------------
 
@@ -108,6 +101,7 @@ INSERT INTO `student` (`id`, `name`, `number`, `email`, `telphone`) VALUES
 CREATE TABLE `teacher` (
   `id` int(11) NOT NULL COMMENT '用户id',
   `name` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(1024) DEFAULT NULL COMMENT '密码',
   `number` varchar(40) NOT NULL COMMENT '编号：老师的编号',
   `email` varchar(40) NOT NULL DEFAULT 'a@b.com' COMMENT '联系方式:email',
   `telphone` varchar(20) NOT NULL COMMENT '联系方式:telphone'
@@ -117,9 +111,11 @@ CREATE TABLE `teacher` (
 -- 转存表中的数据 `teacher`
 --
 
-INSERT INTO `teacher` (`id`, `name`, `number`, `email`, `telphone`) VALUES
-(1, '蔡元培', '001', 'a@b.com', '18000008888'),
-(2, '周树人', '002', 'a@b.com', '18000008888');
+INSERT INTO `teacher` (`id`, `name`, `password`, `number`, `email`, `telphone`) VALUES
+(1, '蔡元培', '', '20170301', '1@sch.edu', '1800000001'),
+(2, '周树人', '', '20170302', '2@sch.edu', '1800000002'),
+(3, '王老师', 'password', '20170303', '3@sch.edu', '1800000003'),
+(4, '李老师', NULL, '20170304', '4@sch.edu', '1800000004');
 
 -- --------------------------------------------------------
 
@@ -139,8 +135,8 @@ CREATE TABLE `test_paper` (
 --
 
 INSERT INTO `test_paper` (`id`, `name`, `detail`, `course_id`) VALUES
-(1, '数学基础期中试卷', '{}', 1),
-(2, '数学基础期末试卷', '{}', 1);
+(2, '2017物理期末试卷', '{\"paper_name\":\"2017物理期末试卷\",\"paper_total_score\":\"100\",\"items\":[{\"type\":\"选择题\",\"data\":[{\"name\":1,\"level_index\":\"1\",\"level_str\":\"基础题目\",\"score\":\"20\"},{\"name\":1,\"level_index\":\"3\",\"level_str\":\"高难度题目\",\"score\":\"30\"}]},{\"type\":\"计算题\",\"data\":[{\"name\":1,\"level_index\":\"2\",\"level_str\":\"中等难度\",\"score\":\"20\"},{\"name\":1,\"level_index\":\"3\",\"level_str\":\"高难度题目\",\"score\":\"30\"}]}]}', 14),
+(1, '2017数学期末考试', '{\"paper_name\":\"2017数学期末考试\",\"paper_total_score\":\"120\",\"items\":[{\"type\":\"选择题\",\"data\":[{\"name\":1,\"level_index\":\"1\",\"level_str\":\"基础题目\",\"score\":\"5\"},{\"name\":1,\"level_index\":\"1\",\"level_str\":\"基础题目\",\"score\":\"5\"},{\"name\":1,\"level_index\":\"1\",\"level_str\":\"基础题目\",\"score\":\"5\"},{\"name\":1,\"level_index\":\"2\",\"level_str\":\"中等难度\",\"score\":\"15\"},{\"name\":1,\"level_index\":\"3\",\"level_str\":\"高难度题目\",\"score\":\"20\"}]},{\"type\":\"填空题\",\"data\":[{\"name\":1,\"level_index\":\"1\",\"level_str\":\"基础题目\",\"score\":\"10\"},{\"name\":1,\"level_index\":\"2\",\"level_str\":\"中等难度\",\"score\":\"10\"}]},{\"type\":\"简答题\",\"data\":[{\"name\":1,\"level_index\":\"1\",\"level_str\":\"基础题目\",\"score\":\"10\"},{\"name\":1,\"level_index\":\"3\",\"level_str\":\"高难度题目\",\"score\":\"20\"}]}]}', 1);
 
 --
 -- Indexes for dumped tables
@@ -185,27 +181,27 @@ ALTER TABLE `test_paper`
 -- 使用表AUTO_INCREMENT `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '键值', AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '键值', AUTO_INCREMENT=16;
 --
 -- 使用表AUTO_INCREMENT `score_result`
 --
 ALTER TABLE `score_result`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键', AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键', AUTO_INCREMENT=14;
 --
 -- 使用表AUTO_INCREMENT `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id', AUTO_INCREMENT=13;
 --
 -- 使用表AUTO_INCREMENT `teacher`
 --
 ALTER TABLE `teacher`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id', AUTO_INCREMENT=5;
 --
 -- 使用表AUTO_INCREMENT `test_paper`
 --
 ALTER TABLE `test_paper`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '试卷对应的ID', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '试卷对应的ID', AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
