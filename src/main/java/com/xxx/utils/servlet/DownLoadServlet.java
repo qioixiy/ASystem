7073c569aeedecd2e6bd100682032418616b2f19
qioixiy <qioixiy@gmail.com>
2017-04-21 23:18:30 +0800

    DownLoadServlet更新

--- src/main/java/com/xxx/utils/servlet/DownLoadServlet.java
+++ src/main/java/com/xxx/utils/servlet/DownLoadServlet.java
@@ -18,78 +18,42 @@ import com.xxx.misc.json.ResponseJsonUtils;
 
 @WebServlet("/utils/DownLoad")
 public class DownLoadServlet extends HttpServlet {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        //得到要下载的文件名
-        String fileName = request.getParameter("filename");
-        fileName = new String(fileName.getBytes("iso8859-1"),"UTF-8");
-        //上传的文件都是保存在/WEB-INF/upload目录下的子目录当中
-        String fileSaveRootPath=this.getServletContext().getRealPath("/WEB-INF/upload");
-        //通过文件名找出文件的所在目录
-        String path = findFileSavePathByFileNameDiret(fileName,fileSaveRootPath);
-        //得到要下载的文件
-        File file = new File(path + "/" + fileName);
-        //如果文件不存在
-        if(!file.exists()){
-        	Map<String, Object> data = new HashMap<String, Object>();
-        	data.put("message", "资源不存在");
-        	ResponseJsonUtils.json(response, data);
-            return;
-        }
-        //处理文件名
-        String realname = fileName.substring(fileName.indexOf("_")+1);
-        //设置响应头，控制浏览器下载该文件
-        response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(realname, "UTF-8"));
-        //读取要下载的文件，保存到文件输入流
-        FileInputStream in = new FileInputStream(path + "/" + fileName);
-        //创建输出流
-        OutputStream out = response.getOutputStream();
-        //创建缓冲区
-        byte buffer[] = new byte[1024];
-        int len = 0;
-        //循环将输入流中的内容读取到缓冲区当中
-        while((len=in.read(buffer))>0){
-            //输出缓冲区的内容到浏览器，实现文件下载
-            out.write(buffer, 0, len);
-        }
-        //关闭文件输入流
-        in.close();
-        //关闭输出流
-        out.close();
-    }
-    
-    /**
-    * @Method: findFileSavePathByFileName
-    * @Description: 通过文件名和存储上传文件根目录找出要下载的文件的所在路径
-    * @Anthor:孤傲苍狼
-    * @param filename 要下载的文件名
-    * @param saveRootPath 上传文件保存的根目录，也就是/WEB-INF/upload目录
-    * @return 要下载的文件的存储目录
-    */ 
-    public String findFileSavePathByFileName(String filename,String saveRootPath){
-        int hashcode = filename.hashCode();
-        int dir1 = hashcode&0xf;  //0--15
-        int dir2 = (hashcode&0xf0)>>4;  //0-15
-        String dir = saveRootPath + "/" + dir1 + "/" + dir2;  //upload\2\3  upload\3\5
-        File file = new File(dir);
-        if(!file.exists()){
-            //创建目录
-            file.mkdirs();
-        }
-        return dir;
-    }
-    
-    public String findFileSavePathByFileNameDiret(String filename,String saveRootPath){
-        return saveRootPath + "/" + filename;
-    }
-    
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String fileName = request.getParameter("filename");
+			fileName = new String(fileName.getBytes("iso8859-1"), "UTF-8");
+			String fileSaveRootPath = this.getServletContext().getRealPath("/WEB-INF");
+			String path = fileSaveRootPath + "/upload";
+			String filepath = path + "/" + fileName;
+			File file = new File(filepath);
+			System.out.println(filepath);
+			if (!file.exists()) {
+				Map<String, Object> data = new HashMap<String, Object>();
+				data.put("message", "资源不存在");
+				ResponseJsonUtils.json(response, data);
+				return;
+			}
+			String realname = fileName.substring(fileName.indexOf("_") + 1);
+			response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(realname, "UTF-8"));
+			FileInputStream in = new FileInputStream(path + "/" + fileName);
+			OutputStream out = response.getOutputStream();
+			byte buffer[] = new byte[1024];
+			int len = 0;
+			while ((len = in.read(buffer)) > 0) {
+				out.write(buffer, 0, len);
+			}
+			in.close();
+			out.close();
+		} catch (Exception e) {
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("error", e.toString());
+			ResponseJsonUtils.json(response, data);
+		}
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
